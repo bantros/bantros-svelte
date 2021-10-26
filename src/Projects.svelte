b875c399ad6fe5ac100dafb6aa4cf65ce5fe09ae
Jonathon Halliwell <jon@bantros.net>
2021-10-26 18:55:46 +0100

    Update projects data and global.css

--- src/Projects.svelte
+++ src/Projects.svelte
@@ -8,6 +8,24 @@
 
   const projects: Projects[] = [
     {
+      id: 'faded',
+      title: 'Faded',
+      url: 'https://fadedstore.com/',
+      color: 'light-red-hover'
+    },
+    {
+      id: 'astonish',
+      title: 'Astonish',
+      url: 'https://astonish.co.uk/',
+      color: 'light-blue-hover'
+    },
+    {
+      id: 'The Reader',
+      title: 'Trails & Tales',
+      url: 'https://trails.thereader.org.uk/',
+      color: 'orange-hover'
+    },
+    {
       id: 'zoflora',
       title: 'Zoflora',
       url: 'https://www.zoflora.co.uk/',
@@ -26,52 +44,10 @@
       color: 'purple-hover'
     },
     {
-      id: 'benenox',
-      title: 'Benenox',
-      url: 'https://www.benenox.com/',
-      color: 'light-purple-hover'
-    },
-    {
-      id: 'turtle-wax',
-      title: 'Turtle Wax',
-      url: 'https://www.turtlewax.com/',
-      color: 'green-hover'
-    },
-    {
-      id: 'northern-lights',
-      title: 'Northern Lights Agency',
-      url: 'http://northernlightsagency.co.uk/',
-      color: 'light-red-hover'
-    },
-    {
-      id: 'mira',
-      title: 'Mira Technology Park',
-      url: 'https://www.miratechnologypark.com/',
-      color: 'light-green-hover'
-    },
-    {
       id: 'raw',
       title: 'Raw Design Studio',
       url: 'https://weareraw.co.uk/',
       color: 'light-yellow-hover'
-    },
-    {
-      id: 'ecb',
-      title: 'England and Wales Cricket Board',
-      url: 'http://getthegameon.co.uk/',
-      color: 'dark-blue-hover'
-    },
-    {
-      id: 'empirical-search',
-      title: 'Empirical Search',
-      url: 'http://empiricalsearch.com/',
-      color: 'blue-hover'
-    },
-    {
-      id: 'woodwork-music',
-      title: 'Woodwork Music',
-      url: 'http://woodworkmusic.co.uk/',
-      color: 'orange-hover'
     }
   ];
 
