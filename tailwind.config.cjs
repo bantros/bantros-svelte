b3b72004a024c11d3f2f0e8647bd1f0beb4a0052
Jonathon Halliwell <jon@bantros.net>
2022-06-11 17:57:44 +0100

    Add custom system fontFamily to theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,svelte}'],
   theme: {
+    fontFamily: {
+      'system': ['-apple-system', 'BlinkMacSystemFont', 'avenir', 'helvetica neue', 'helvetica', 'ubuntu', 'roboto', 'noto', 'segoe ui', 'arial', 'sans-serif']
+    },
     extend: {}
   },
   plugins: []
