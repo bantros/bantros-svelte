50b457bbe3f5c16d2929ef22f5b6b9a785d4c2a2
Jonathon Halliwell <jon@bantros.net>
2022-06-11 18:06:33 +0100

    Remove avenir from system fontFamily

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,svelte}'],
   theme: {
     fontFamily: {
-      'system': ['-apple-system', 'BlinkMacSystemFont', 'avenir', 'helvetica neue', 'helvetica', 'ubuntu', 'roboto', 'noto', 'segoe ui', 'arial', 'sans-serif']
+      'system': ['-apple-system', 'BlinkMacSystemFont', 'helvetica neue', 'helvetica', 'ubuntu', 'roboto', 'noto', 'segoe ui', 'arial', 'sans-serif']
     },
     extend: {}
   },
